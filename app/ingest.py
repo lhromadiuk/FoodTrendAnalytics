f2b87e01460327edce4783815fb1a053792642c3
Liubomyr Hromadiuk <lihr00001@stud.uni-saarland.de>
2025-05-07 00:12:42 +0200

    adjusted ingestion

--- app/ingest.py
+++ app/ingest.py
@@ -5,30 +5,37 @@ from flask.cli import with_appcontext
 from .database import db
 from .models import Recipe, Ingredient
 from .search import index_recipe
+import string
+
 
 @click.command('ingest-data')
 @with_appcontext
 def ingest_data():
     """Fetch recipes from API and store + index them"""
-    url = current_app.config['MEALDB_URL']
-    meals = requests.get(url).json().get('meals') or []
-    for item in meals:
-        rid = int(item['idMeal'])
-        if not Recipe.query.get(rid):
-            r = Recipe(
-                id=rid,
-                title=item['strMeal'],
-                cuisine=item['strArea'],
-                instructions=item.get('strInstructions',''),
-                image = item.get('strMealThumb','')
-            )
-            db.session.add(r)
-            for i in range(1, 21):
-                name = item.get(f'strIngredient{i}')
-                if name:
-                    ing = Ingredient.query.filter_by(name=name).first() or Ingredient(name=name)
-                    db.session.add(ing)
-                    r.ingredients.append(ing)
-            db.session.commit()
-            index_recipe(r)
-    click.echo('Ingestion complete')
+    base_url = current_app.config['MEALDB_URL']
+    total_added = 0
+    for letter in string.ascii_lowercase:
+        url = f"{base_url}{letter}"
+
+        meals = requests.get(url).json().get('meals') or []
+        for item in meals:
+            rid = int(item['idMeal'])
+            if not Recipe.query.get(rid):
+                r = Recipe(
+                    id=rid,
+                    title=item['strMeal'],
+                    cuisine=item['strArea'],
+                    instructions=item.get('strInstructions',''),
+                    image_url = item.get('strMealThumb','')
+                )
+                db.session.add(r)
+                for i in range(1, 21):
+                    name = item.get(f'strIngredient{i}').title()
+                    if name:
+                        ing = Ingredient.query.filter_by(name=name).first() or Ingredient(name=name)
+                        db.session.add(ing)
+                        r.ingredients.append(ing)
+                db.session.commit()
+                index_recipe(r)
+                total_added += 1
+    click.echo(f'Ingestion complete, {total_added} recipes added')
