83e90b30b5eac3dbab764f0d482bebe1e651a7de
Liubomyr Hromadiuk <lihr00001@stud.uni-saarland.de>
2025-05-07 14:08:32 +0200

    adjusted search to account for cuisine/ingridients

--- app/search.py
+++ app/search.py
@@ -6,10 +6,9 @@ ES_INDEX = 'recipes'
 
 
 def get_es_client():
-
     cfg = current_app.config
     endpoint = cfg.get('ELASTIC_ENDPOINT')
-    api_key   = cfg.get('ELASTIC_API_KEY')
+    api_key = cfg.get('ELASTIC_API_KEY')
 
     if not endpoint or not api_key:
         raise RuntimeError("ENDPOINT and API_KEY must be set")
@@ -18,35 +17,50 @@ def get_es_client():
 
 
 def init_index(es):
-
     if not es.indices.exists(index=ES_INDEX):
         es.indices.create(index=ES_INDEX, body={
             'mappings': {
                 'properties': {
-                    'title':        {'type': 'text'},
-                    'cuisine':      {'type': 'keyword'},
-                    'instructions': {'type': 'text'},
-                    'ingredients':  {'type': 'keyword'},
-                    'image_url': {'type': 'keyword'}
+                    'title': {
+                        'type': 'text'
+                    },
+                    'cuisine': {
+                        'type': 'text',
+                        'fields': {
+                            'raw': {'type': 'keyword'}
+                        }
+                    },
+                    'instructions': {
+                        'type': 'text'
+                    },
+                    'ingredients': {
+                        'type': 'text',
+                        'fields': {
+                            'raw': {'type': 'keyword'}
+                        }
+                    },
+                    'image_url': {
+                        'type': 'keyword'
+                    }
                 }
             }
+
         })
 
 
 def index_recipe(recipe):
-
     es = get_es_client()
     init_index(es)
     doc = {
-        'title':        recipe.title,
-        'cuisine':      recipe.cuisine,
+        'title': recipe.title,
+        'cuisine': recipe.cuisine,
         'instructions': recipe.instructions,
-        'ingredients':  [ingredient.name for ingredient in recipe.ingredients]
+        'ingredients': [ingredient.name for ingredient in recipe.ingredients]
     }
     es.index(index=ES_INDEX, id=recipe.id, document=doc)
 
 
-def search_recipes(query, cuisine=None, ingredient=None):
+def search_recipes(query):
     es = get_es_client()
     init_index(es)
 
@@ -56,23 +70,24 @@ def search_recipes(query, cuisine=None, ingredient=None):
                 'must': {
                     'multi_match': {
                         'query': query,
-                        'fields': ['title', 'instructions']
+                        'fields': [
+                            'title^3',
+                            'instructions^1',
+                            'cuisine^5',
+                            'ingredients^2'
+                        ],
                     }
                 }
             }
         }
     }
-    if cuisine:
-        body['query']['bool'].setdefault('filter', []).append({'term': {'cuisine': cuisine}})
-    if ingredient:
-        body['query']['bool'].setdefault('filter', []).append({'term': {'ingredients': ingredient}})
-
     res = es.search(index=ES_INDEX, body=body)
     return [
         {
             "id": hit["_id"],
             "title": hit["_source"]["title"],
             "cuisine": hit["_source"]["cuisine"],
+            "image_url": hit["_source"].get("image_url"),
             "score": hit["_score"]
         }
         for hit in res["hits"]["hits"]
